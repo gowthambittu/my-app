cc12f40c9c90767e5d257b1c7be16983ffcec9b7
Bittu, Gowtham (Contractor) <Gowtham_Bittu@comcast.com>
2023-09-12 13:52:57 -0400

    changes in contact js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -53,6 +53,7 @@ const handleSubmit = async (e) => {
   try {
     const response = await fetch('https://my-app-backend-3d9285b8e62a.herokuapp.com/send-email', {
       method: 'POST',
+      mode: 'cors',
       headers: {
         'Content-Type': 'application/json',
       },
